357b3d4633a1117b99a76d7c0235e04addfda785
Mwansa Pembanyali <mwansap07@gmail.com>
2021-02-19 00:18:02 +0200

    chore: added pages for dashboard, login, profile, settings, summary and timeclock pages

---
+++ TimeTrackerTutorial/Pages/SummaryPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TimeTrackerTutorial.Pages
+{
+    public partial class SummaryPage : ContentPage
+    {
+        public SummaryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
