4de6ec586613728eb1ae637df0232154b8ebffea
Mwansa Pembanyali <mwansap07@gmail.com>
2021-02-19 00:09:30 +0200

    feat: added SetProperty method in PageModelBase class to allow for notifying Xamarin that a property has changed

--- TimeTrackerTutorial/PageModels/Base/PageModelBase.cs
+++ TimeTrackerTutorial/PageModels/Base/PageModelBase.cs
@@ -1,10 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Xamarin.Forms;
 
 namespace TimeTrackerTutorial.PageModels.Base
 {
     public class PageModelBase : BindableObject
     {
-        
+        string _title;
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        bool _isLoading;
+        public bool IsLoading
+        {
+            get => _isLoading;
+            set => SetProperty(ref _isLoading, value);
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
     }
 }
