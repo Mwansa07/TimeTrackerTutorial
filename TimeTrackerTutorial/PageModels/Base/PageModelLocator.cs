4cbcc083a7f220ac9e00c6c8347ee2a66ddcf561
Mwansa Pembanyali <mwansap07@gmail.com>
2021-02-18 23:40:30 +0200

    feat: created Resolve method for returning an instance of a page model. created a method to create a page, set its binding context to a page model and  returned the page.

--- TimeTrackerTutorial/PageModels/Base/PageModelLocator.cs
+++ TimeTrackerTutorial/PageModels/Base/PageModelLocator.cs
@@ -1,8 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using TinyIoC;
+using Xamarin.Forms;
+
 namespace TimeTrackerTutorial.PageModels.Base
 {
     public class PageModelLocator
     {
-        
+        static TinyIoCContainer _container;
+        static Dictionary<Type, Type> _viewLookup;
+
+        static PageModelLocator()
+        {
+            _container = new TinyIoCContainer();
+            _viewLookup = new Dictionary<Type, Type>();
+
+            //Register Pages and Page Models
+
+
+            //Register Services (services are registered as singletons by default)
+        }
+
+        public static T Resolve<T>() where T : class
+        {
+            return _container.Resolve<T>();
+        }
+
+        public static Page CreatePageFor(Type pageModelType)
+        {
+            var pageType = _viewLookup[pageModelType];
+            var page = (Page)Activator.CreateInstance(pageType);
+            var pageModel = _container.Resolve(pageModelType);
+            page.BindingContext = pageModel;
+            return page;
+        }
     }
 }
