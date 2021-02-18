6d5ef47d48c5f5c1274dbd261d7a0f2d9c97ee41
Mwansa Pembanyali <mwansap07@gmail.com>
2021-02-19 00:51:01 +0200

    feat: completed the NavigateToAsync method

--- TimeTrackerTutorial/Services/Navigation/NavigationService.cs
+++ TimeTrackerTutorial/Services/Navigation/NavigationService.cs
@@ -1,10 +1,41 @@
 ﻿using System;
+using System.Threading.Tasks;
+using TimeTrackerTutorial.PageModels.Base;
+using Xamarin.Forms;
+
 namespace TimeTrackerTutorial.Services.Navigation
 {
-    public class NavigationService
+    public class NavigationService : INavigationService
     {
         public NavigationService()
         {
+        }
+
+        public Task GoBackAsync()
+        {
+            return App.Current.MainPage.Navigation.PopAsync();
+        }
+
+        public Task NavigateToAsync<TPageModelBase>(object navigationData = null, bool setRoot = false)
+        {
+            var page = PageModelLocator.CreatePageFor(typeof(TPageModelBase));
+
+            if (setRoot)
+            {
+                App.Current.MainPage = new NavigationPage(page);
+            }
+            else
+            {
+                if (App.Current.MainPage is NavigationPage navPage)
+                {
+                    return navPage.PushAsync(page);
+                }
+                else
+                {
+                    App.Current.MainPage = new NavigationPage(page);
+                }
+            }
+            return Task.CompletedTask;
         }
     }
 }
