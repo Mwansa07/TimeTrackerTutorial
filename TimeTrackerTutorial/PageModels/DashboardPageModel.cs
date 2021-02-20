69f4c66dd7c078ddcf60623475f487f3e5b51c56
Mwansa Pembanyali <mwansap07@gmail.com>
2021-02-21 00:12:06 +0200

    feat: added properties for all page models in the dashboardPageModel.

--- TimeTrackerTutorial/PageModels/DashboardPageModel.cs
+++ TimeTrackerTutorial/PageModels/DashboardPageModel.cs
@@ -5,8 +5,39 @@ namespace TimeTrackerTutorial.PageModels
 {
     public class DashboardPageModel : PageModelBase
     {
-        public DashboardPageModel()
+        private LoginPageModel _loginPM;
+        public LoginPageModel LoginPageModel
         {
+            get => _loginPM;
+            set => SetProperty(ref _loginPM, value);
+        }
+
+        private ProfilePageModel _profilePM;
+        public ProfilePageModel ProfilePageModel
+        {
+            get => _profilePM;
+            set => SetProperty(ref _profilePM, value);
+        }
+
+        private SettingsPageModel _settingsPM;
+        public SettingsPageModel SettingsPageModel
+        {
+            get => _settingsPM;
+            set => SetProperty(ref _settingsPM, value);
+        }
+
+        private SummaryPageModel _summaryPM;
+        public SummaryPageModel SummaryPageModel
+        {
+            get => _summaryPM;
+            set => SetProperty(ref _summaryPM, value);
+        }
+
+        private TimeClockPageModel _timePM;
+        public TimeClockPageModel TimeClockPageModel
+        {
+            get => _timePM;
+            set => SetProperty(ref _timePM, value);
         }
     }
 }
