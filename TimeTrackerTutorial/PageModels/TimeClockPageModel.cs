61882a68310ed47d7d03de501376d9f5fe44c58d
Mwansa Pembanyali <mwansap07@gmail.com>
2021-02-21 03:45:56 +0200

    feat: create, running total and current start time properties in the timeclock page

--- TimeTrackerTutorial/PageModels/TimeClockPageModel.cs
+++ TimeTrackerTutorial/PageModels/TimeClockPageModel.cs
@@ -5,8 +5,18 @@ namespace TimeTrackerTutorial.PageModels
 {
     public class TimeClockPageModel : PageModelBase
     {
-        public TimeClockPageModel()
+        TimeSpan _runningTotal;
+        public TimeSpan RunningTotal
         {
+            get => _runningTotal;
+            set => SetProperty(ref _runningTotal, value);
+        }
+
+        DateTime _currentStartTime;
+        public DateTime CurrentStartTime
+        {
+            get => _currentStartTime;
+            set => SetProperty(ref _currentStartTime, value);
         }
     }
 }
