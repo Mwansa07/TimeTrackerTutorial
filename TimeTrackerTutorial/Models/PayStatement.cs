c477be6aa3fe1a05d81166ef16b5c5203558a35d
Mwansa Pembanyali <mwansap07@gmail.com>
2021-02-23 22:40:25 +0200

    feat: created PayStatement model.

---
+++ TimeTrackerTutorial/Models/PayStatement.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TimeTrackerTutorial.Models
+{
+    public class PayStatement
+    {
+        /// <summary>
+        /// Pay Period Start Date
+        /// </summary>
+        public DateTime Start { get; set; }
+
+        /// <summary>
+        /// Pay Period End Date
+        /// </summary>
+        public DateTime End { get; set; }
+
+        /// <summary>
+        /// Payout Date for Period
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Payout Amount
+        /// </summary>
+        public double Amount { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public List<WorkItem> WorkItems { get; set; }
+    }
+}
--- TimeTrackerTutorial/PageModels/SummaryPageModel.cs
+++ TimeTrackerTutorial/PageModels/SummaryPageModel.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using TimeTrackerTutorial.PageModels.Base;
 
 namespace TimeTrackerTutorial.PageModels
 {
     public class SummaryPageModel : PageModelBase
     {
-        public SummaryPageModel()
-        {
-        }
+        public string CurrentPayDateRange { get; set; }
+        public double CurrentPeriodEarnings { get; set; }
+        public DateTime CurrentPeriodPayDate { get; set; }
+        public List<object> Statements { get; set; }
     }
 }
